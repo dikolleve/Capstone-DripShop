0cda5d10bca4d5cac8a43a276b4ad8cdd9ff1888
Diko Lleve <diko.lleve09@gmail.com>
2025-05-21 07:01:22 -0700

    creating code to index.ejs to display products

--- index.js
+++ index.js
@@ -9,9 +9,22 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const API_URL = "https://fakestoreapi.com";
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+
+app.get("/", async (req, res) => {
+    try {
+        const productRes = await axios.get(`${API_URL}/products`);
+        res.render("index", {
+            products: productRes.data
+        });
+    } catch (error) {
+        console.error("Error to fetch products: ", error.message);
+        res.status(500).send("Failed to fetch products");
+    }
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
